2fba32054bbb83a6343ff4e1225417db9e6f743f
Brian <bduff9@gmail.com>
2016-02-14 13:27:36 -0600

    Create ReactSkeleton.js

---
+++ skeletons/ReactSkeleton.js
@@ -0,0 +1,27 @@
+React.createClass({
+
+  // Define class props and mixins
+  //mixins: [],
+  //propTypes: {},
+  //getDefaultProps: function() {},
+
+  // Mounting events
+  //getInitialState: function() {},
+  //componentWillMount: function() {},
+  //componentDidMount: function() {},
+
+  // Updating events
+  //componentWillReceiveProps: function(nextProps) {},
+  //shouldComponentUpdate: function(nextProps, nextState) {},
+  //componentWillUpdate: function(nextProps, nextState) {},
+  //componentDidUpdate: function(prevProps, prevState) {},
+
+  // Unmounting events
+  //componentWillUnmount : function() {},
+
+  // Custom functions
+  //_myFunc: function() {},
+
+  render: function() {}
+
+});
